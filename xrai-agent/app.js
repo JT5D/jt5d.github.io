b826eca0d5905f399587d5fcfa320019929b95ff
JT5D <jtunick@theimclab.com>
2026-09-13 00:00:23 -0400

    Pin XRAI app to v0.3.4 source

--- xrai-agent/app.js
+++ xrai-agent/app.js
@@ -1,1 +1,1 @@
-await import('https://cdn.jsdelivr.net/gh/JT5D/xrai-agent@bff6c9a267c16bef8fe169c19511f57c9a967677/web/app.js');
+await import('https://cdn.jsdelivr.net/gh/JT5D/xrai-agent@25ec512627feccaa341ac384af38f338bf5fbc48/web/app.js');
